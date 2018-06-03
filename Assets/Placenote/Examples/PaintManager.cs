f1a52c386ba3383e10da767594d1f2cfbc5ee050
David Fierstein <studio@davidiad.com>
2018-06-03 10:42:05 -0700

    Add Unity project

---
+++ Assets/Placenote/Examples/PaintManager.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+using UnityEngine.UI;
+
+public class PaintManager : MonoBehaviour
+{
+    public Button onoff;
+    public GameObject paintTarget;
+    private Mesh mesh; // save particles in a mesh
+
+    public ParticleSystem particleSystemTemplate;
+
+    private bool newPaintVertices;
+    private bool paintingOn;
+    private Color paintColor;
+    private Vector3 previousPosition;
+
+    public List<ParticleSystem> particleSystemList; // Stores all particle systems
+    public List<Vector3> currVertices; // Stores current camera positions to paint
+    public ParticleSystem ps; // Stores current particle system
+
+    [SerializeField] Camera mainCam;
+
+    void OnEnable()
+    {
+        //UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
+    }
+
+    void OnDestroy()
+    {
+        //UnityARSessionNativeInterface.ARFrameUpdatedEvent -= ARFrameUpdated;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        paintingOn = false;
+        newPaintVertices = false;
+        particleSystemList = new List<ParticleSystem>();
+        ps = Instantiate(particleSystemTemplate);
+        currVertices = new List<Vector3>();
+        paintColor = Color.green;
+        mesh = new Mesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paintingOn && newPaintVertices)
+        {
+            if (currVertices.Count > 0)
+            {
+                ParticleSystem.Particle[] particles = new ParticleSystem.Particle[currVertices.Count];
+                int index = 0;
+                foreach (Vector3 vtx in currVertices)
+                {
+                    particles[index].position = vtx;
+                    particles[index].color = paintColor;
+                    particles[index].size = 0.05f;
+                    index++;
+                }
+                ps.SetParticles(particles, currVertices.Count);
+                newPaintVertices = false;
+            }
+        }
+        Paint();
+    }
+
+    public void TogglePaint()
+    {
+        paintingOn = !paintingOn;
+        if (paintingOn)
+        {
+            onoff.transform.localScale = new Vector3(1.7f, 1.7f, 1.7f);
+        }
+        else
+        {
+            onoff.transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    public void RandomizeColor()
+    {
+        if (ps.particleCount > 0)
+        {
+            SaveParticleSystem();
+        }
+        paintColor = Random.ColorHSV();
+    }
+
+    public void Reset()
+    {
+        foreach (ParticleSystem p in particleSystemList)
+        {
+            Destroy(p);
+        }
+        particleSystemList = new List<ParticleSystem>();
+
+        Destroy(ps);
+        ps = Instantiate(particleSystemTemplate);
+        currVertices = new List<Vector3>();
+    }
+
+    private void SaveParticleSystem()
+    {
+        particleSystemList.Add(ps);
+        ps = Instantiate(particleSystemTemplate);
+        currVertices = new List<Vector3>();
+    }
+
+    public Mesh GetMesh()
+    {
+        // get all the particles, and save them in a mesh
+        //foreach (ParticleSystem partSys in particleSystemList)
+        //{
+        /*
+        ParticleSystem.Particle[] myParticles = (ParticleSystem)GetComponent("ParticleSystem");
+        partSys.GetParticles(myParticles);
+        foreach (Particle particle in myParticles)
+        {
+
+        }
+
+        ParticleSystem.Particle[] currentParticles = new ParticleSystem.Particle[partSys.particleCount]; 
+        partSys.GetParticles(currentParticles);
+        foreach (Particle particle in currentParticles) 
+        {
+
+        }
+        Vector3 [] verts = partSys.GetParticles()
+
+    }
+    */
+        return mesh;
+    }
+
+    private void Paint()
+    {
+        Vector3 paintPosition = paintTarget.transform.position;
+        //Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane)) + Camera.main.transform.forward * 2.0f;
+        //Camera.main.transform.position + Camera.main.transform.forward * 0.3f;
+        if (Vector3.Distance(paintPosition, previousPosition) > 0.025f)
+        {
+            if (paintingOn) currVertices.Add(paintPosition);
+            previousPosition = paintPosition;
+            newPaintVertices = true;
+
+        }
+    }
+    // Quaternion rot = Camera.main.transform.rotation;
+    /*
+    private void ARFrameUpdated(UnityARCamera arCamera)
+    {
+
+        Vector3 paintPosition = (Camera.main.transform.forward * 0.2f) + GetCameraPosition(arCamera);
+        if (Vector3.Distance(paintPosition, previousPosition) > 0.025f)
+        {
+            if (paintingOn) currVertices.Add(paintPosition);
+            previousPosition = paintPosition;
+            newPaintVertices = true;
+            Debug.Log("arCam Position: " + paintPosition);
+        } else {
+            Debug.Log("arCam Position, Painting off: " + paintPosition);
+        }
+    }
+    */
+
+    private Vector3 GetCameraPosition(UnityARCamera cam)
+    {
+        Matrix4x4 matrix = new Matrix4x4();
+        matrix.SetColumn(3, cam.worldTransform.column3);
+        return UnityARMatrixOps.GetPosition(matrix);
+    }
+}
---
+++ Assets/Placenote/Examples/PlacenoteSampleVi.cs
@@ -0,0 +1,563 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.XR.iOS;
+using System.Runtime.InteropServices;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+/*
+[System.Serializable]
+public class ShapeInfo
+{
+    public float px;
+    public float py;
+    public float pz;
+    public float qx;
+    public float qy;
+    public float qz;
+    public float qw;
+    public int shapeType;
+}
+*/
+/*
+
+[System.Serializable]
+public class SV3List
+{
+    public SerializableVector3[] sv3s;
+}
+
+
+[System.Serializable]
+public class ShapeList
+{
+    public ShapeInfo[] shapes;
+}
+
+*/
+public class PlacenoteSampleVi : MonoBehaviour, PlacenoteListener
+{
+    [SerializeField] GameObject mMapSelectedPanel;
+    [SerializeField] GameObject mInitButtonPanel;
+    [SerializeField] GameObject mMappingButtonPanel;
+    [SerializeField] GameObject mMapListPanel;
+    [SerializeField] GameObject mExitButton;
+    [SerializeField] GameObject mListElement;
+    [SerializeField] RectTransform mListContentParent;
+    [SerializeField] ToggleGroup mToggleGroup;
+    [SerializeField] GameObject mPlaneDetectionToggle;
+    [SerializeField] Text mLabelText;
+    [SerializeField] Material mShapeMaterial;
+    [SerializeField] PlacenoteARGeneratePlane mPNPlaneManager;
+
+    private UnityARSessionNativeInterface mSession;
+    private bool mFrameUpdated = false;
+    private UnityARImageFrameData mImage = null;
+    private UnityARCamera mARCamera;
+    private bool mARKitInit = false;
+    private List<ShapeInfo> shapeInfoList = new List<ShapeInfo>();
+    private List<GameObject> shapeObjList = new List<GameObject>();
+    private List<SerializableVector3> v3list = new List<SerializableVector3>();
+
+    private PaintManager paintManager;
+
+    private LibPlacenote.MapInfo mSelectedMapInfo;
+    private string mSelectedMapId
+    {
+        get
+        {
+            return mSelectedMapInfo != null ? mSelectedMapInfo.placeId : null;
+        }
+    }
+
+    private BoxCollider mBoxColliderDummy;
+    private SphereCollider mSphereColliderDummy;
+    private CapsuleCollider mCapColliderDummy;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Input.location.Start();
+
+        mMapListPanel.SetActive(false);
+
+        mSession = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+        UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
+        StartARKit();
+        FeaturesVisualizer.EnablePointcloud();
+        LibPlacenote.Instance.RegisterListener(this);
+
+        GameObject pmgo = GameObject.FindWithTag("PaintManager");
+        paintManager = pmgo.GetComponent<PaintManager>();
+    }
+
+
+    private void ARFrameUpdated(UnityARCamera camera)
+    {
+        mFrameUpdated = true;
+        mARCamera = camera;
+    }
+
+
+    private void InitARFrameBuffer()
+    {
+        mImage = new UnityARImageFrameData();
+
+        int yBufSize = mARCamera.videoParams.yWidth * mARCamera.videoParams.yHeight;
+        mImage.y.data = Marshal.AllocHGlobal(yBufSize);
+        mImage.y.width = (ulong)mARCamera.videoParams.yWidth;
+        mImage.y.height = (ulong)mARCamera.videoParams.yHeight;
+        mImage.y.stride = (ulong)mARCamera.videoParams.yWidth;
+
+        // This does assume the YUV_NV21 format
+        int vuBufSize = mARCamera.videoParams.yWidth * mARCamera.videoParams.yWidth / 2;
+        mImage.vu.data = Marshal.AllocHGlobal(vuBufSize);
+        mImage.vu.width = (ulong)mARCamera.videoParams.yWidth / 2;
+        mImage.vu.height = (ulong)mARCamera.videoParams.yHeight / 2;
+        mImage.vu.stride = (ulong)mARCamera.videoParams.yWidth;
+
+        mSession.SetCapturePixelData(true, mImage.y.data, mImage.vu.data);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mFrameUpdated)
+        {
+            mFrameUpdated = false;
+            if (mImage == null)
+            {
+                InitARFrameBuffer();
+            }
+
+            if (mARCamera.trackingState == ARTrackingState.ARTrackingStateNotAvailable)
+            {
+                // ARKit pose is not yet initialized
+                return;
+            }
+            else if (!mARKitInit)
+            {
+                mARKitInit = true;
+                mLabelText.text = "ARKit Initialized";
+            }
+
+            Matrix4x4 matrix = mSession.GetCameraPose();
+
+            Vector3 arkitPosition = PNUtility.MatrixOps.GetPosition(matrix);
+            Quaternion arkitQuat = PNUtility.MatrixOps.GetRotation(matrix);
+
+            LibPlacenote.Instance.SendARFrame(mImage, arkitPosition, arkitQuat,
+                                               mARCamera.videoParams.screenOrientation);
+        }
+    }
+
+
+    public void OnListMapClick()
+    {
+        if (!LibPlacenote.Instance.Initialized())
+        {
+            Debug.Log("SDK not yet initialized");
+            ToastManager.ShowToast("SDK not yet initialized", 2f);
+            return;
+        }
+
+        foreach (Transform t in mListContentParent.transform)
+        {
+            Destroy(t.gameObject);
+        }
+
+        mMapListPanel.SetActive(true);
+        mInitButtonPanel.SetActive(false);
+        LibPlacenote.Instance.ListMaps((mapList) =>
+        {
+            // render the map list!
+            foreach (LibPlacenote.MapInfo mapId in mapList)
+            {
+                if (mapId.userData != null)
+                {
+                    Debug.Log(mapId.userData.ToString(Formatting.None));
+                }
+                AddMapToList(mapId);
+            }
+        });
+    }
+
+
+    public void OnCancelClick()
+    {
+        mMapSelectedPanel.SetActive(false);
+        mMapListPanel.SetActive(false);
+        mInitButtonPanel.SetActive(true);
+    }
+
+
+    public void OnExitClick()
+    {
+        mInitButtonPanel.SetActive(true);
+        mExitButton.SetActive(false);
+        mPlaneDetectionToggle.SetActive(false);
+
+        //clear all existing planes
+        mPNPlaneManager.ClearPlanes();
+        mPlaneDetectionToggle.GetComponent<Toggle>().isOn = false;
+
+        LibPlacenote.Instance.StopSession();
+    }
+
+
+    void AddMapToList(LibPlacenote.MapInfo mapInfo)
+    {
+        GameObject newElement = Instantiate(mListElement) as GameObject;
+        MapInfoElement listElement = newElement.GetComponent<MapInfoElement>();
+        listElement.Initialize(mapInfo, mToggleGroup, mListContentParent, (value) =>
+        {
+            OnMapSelected(mapInfo);
+        });
+    }
+
+
+    void OnMapSelected(LibPlacenote.MapInfo mapInfo)
+    {
+        mSelectedMapInfo = mapInfo;
+        mMapSelectedPanel.SetActive(true);
+    }
+
+
+    public void OnLoadMapClicked()
+    {
+        ConfigureSession(false);
+
+        if (!LibPlacenote.Instance.Initialized())
+        {
+            Debug.Log("SDK not yet initialized");
+            ToastManager.ShowToast("SDK not yet initialized", 2f);
+            return;
+        }
+
+        mLabelText.text = "Loading Map ID: " + mSelectedMapId;
+        LibPlacenote.Instance.LoadMap(mSelectedMapId,
+            (completed, faulted, percentage) =>
+            {
+                if (completed)
+                {
+                    mMapSelectedPanel.SetActive(false);
+                    mMapListPanel.SetActive(false);
+                    mInitButtonPanel.SetActive(false);
+                    mExitButton.SetActive(true);
+                    mPlaneDetectionToggle.SetActive(true);
+
+                    LibPlacenote.Instance.StartSession();
+                    mLabelText.text = "Loaded ID: " + mSelectedMapId;
+                }
+                else if (faulted)
+                {
+                    mLabelText.text = "Failed to load ID: " + mSelectedMapId;
+                }
+            }
+        );
+    }
+
+    public void OnDeleteMapClicked()
+    {
+        if (!LibPlacenote.Instance.Initialized())
+        {
+            Debug.Log("SDK not yet initialized");
+            ToastManager.ShowToast("SDK not yet initialized", 2f);
+            return;
+        }
+
+        mLabelText.text = "Deleting Map ID: " + mSelectedMapId;
+        LibPlacenote.Instance.DeleteMap(mSelectedMapId, (deleted, errMsg) =>
+        {
+            if (deleted)
+            {
+                mMapSelectedPanel.SetActive(false);
+                mLabelText.text = "Deleted ID: " + mSelectedMapId;
+                OnListMapClick();
+            }
+            else
+            {
+                mLabelText.text = "Failed to delete ID: " + mSelectedMapId;
+            }
+        });
+    }
+
+
+
+    public void OnNewMapClick()
+    {
+        ConfigureSession(false);
+
+        if (!LibPlacenote.Instance.Initialized())
+        {
+            Debug.Log("SDK not yet initialized");
+            return;
+        }
+
+        mInitButtonPanel.SetActive(false);
+        mMappingButtonPanel.SetActive(true);
+        mPlaneDetectionToggle.SetActive(true);
+        Debug.Log("Started Session");
+        LibPlacenote.Instance.StartSession();
+    }
+
+    public void OnTogglePlaneDetection()
+    {
+        ConfigureSession(true);
+    }
+
+    private void StartARKit()
+    {
+        mLabelText.text = "Initializing ARKit";
+        Application.targetFrameRate = 60;
+        ConfigureSession(false);
+    }
+
+
+    private void ConfigureSession(bool clearPlanes)
+    {
+        ARKitWorldTrackingSessionConfiguration config =
+            new ARKitWorldTrackingSessionConfiguration();
+
+        if (mPlaneDetectionToggle.GetComponent<Toggle>().isOn)
+        {
+            if (UnityARSessionNativeInterface.IsARKit_1_5_Supported())
+            {
+                config.planeDetection = UnityARPlaneDetection.HorizontalAndVertical;
+            }
+            else
+            {
+                config.planeDetection = UnityARPlaneDetection.Horizontal;
+            }
+            mPNPlaneManager.StartPlaneDetection();
+        }
+        else
+        {
+            config.planeDetection = UnityARPlaneDetection.None;
+            if (clearPlanes)
+            {
+                mPNPlaneManager.ClearPlanes();
+            }
+        }
+
+        config.alignment = UnityARAlignment.UnityARAlignmentGravity;
+        config.getPointCloudData = true;
+        config.enableLightEstimation = true;
+        mSession.RunWithConfig(config);
+    }
+
+
+    public void OnSaveMapClick()
+    {
+        if (!LibPlacenote.Instance.Initialized())
+        {
+            Debug.Log("SDK not yet initialized");
+            ToastManager.ShowToast("SDK not yet initialized", 2f);
+            return;
+        }
+
+        bool useLocation = Input.location.status == LocationServiceStatus.Running;
+        LocationInfo locationInfo = Input.location.lastData;
+
+        mLabelText.text = "Saving...";
+        LibPlacenote.Instance.SaveMap(
+            (mapId) =>
+            {
+                LibPlacenote.Instance.StopSession();
+                mLabelText.text = "Saved Map ID: " + mapId;
+                mInitButtonPanel.SetActive(true);
+                mMappingButtonPanel.SetActive(false);
+                mPlaneDetectionToggle.SetActive(false);
+
+                //clear all existing planes
+                mPNPlaneManager.ClearPlanes();
+                mPlaneDetectionToggle.GetComponent<Toggle>().isOn = false;
+
+
+                JObject metadata = new JObject();
+                JObject shapeList = Shapes2JSON();
+                metadata["shapeList"] = shapeList;
+
+                JObject sv3list = Sv3s2JSON();
+                metadata["sv3list"] = sv3list;
+
+                if (useLocation)
+                {
+                    metadata["location"] = new JObject();
+                    metadata["location"]["latitude"] = locationInfo.latitude;
+                    metadata["location"]["longitude"] = locationInfo.longitude;
+                    metadata["location"]["altitude"] = locationInfo.altitude;
+                }
+                LibPlacenote.Instance.SetMetadata(mapId, metadata);
+            },
+            (completed, faulted, percentage) => { }
+        );
+    }
+
+
+    public void OnDropShapeClick()
+    {
+        Vector3 shapePosition = Camera.main.transform.position +
+                                      Camera.main.transform.forward * 0.3f;
+        Quaternion shapeRotation = Camera.main.transform.rotation;
+
+        System.Random rnd = new System.Random();
+        PrimitiveType type = (PrimitiveType)rnd.Next(0, 3);
+
+        ShapeInfo shapeInfo = new ShapeInfo();
+        shapeInfo.px = shapePosition.x;
+        shapeInfo.py = shapePosition.y;
+        shapeInfo.pz = shapePosition.z;
+        shapeInfo.qx = shapeRotation.x;
+        shapeInfo.qy = shapeRotation.y;
+        shapeInfo.qz = shapeRotation.z;
+        shapeInfo.qw = shapeRotation.w;
+        shapeInfo.shapeType = type.GetHashCode();
+        shapeInfoList.Add(shapeInfo);
+
+        GameObject shape = ShapeFromInfo(shapeInfo);
+        shapeObjList.Add(shape);
+    }
+
+
+    private GameObject ShapeFromInfo(ShapeInfo info)
+    {
+        GameObject shape = GameObject.CreatePrimitive((PrimitiveType)info.shapeType);
+        shape.transform.position = new Vector3(info.px, info.py, info.pz);
+        shape.transform.rotation = new Quaternion(info.qx, info.qy, info.qz, info.qw);
+        shape.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        shape.GetComponent<MeshRenderer>().material = mShapeMaterial;
+
+        return shape;
+    }
+
+
+    private void ClearShapes()
+    {
+        foreach (var obj in shapeObjList)
+        {
+            Destroy(obj);
+        }
+        shapeObjList.Clear();
+        shapeInfoList.Clear();
+    }
+
+    private JObject Sv3s2JSON()
+    {
+        SV3List sV3List = new SV3List();
+        sV3List.sv3s = new SerializableVector3[paintManager.currVertices.Count];
+        for (int i = 0; i < paintManager.currVertices.Count; i++)
+        {
+            sV3List.sv3s[i] = paintManager.currVertices[i];
+        }
+        sV3List.sv3s = new SerializableVector3[4];
+        sV3List.sv3s[0] = new SerializableVector3(1, 2, 3);
+        sV3List.sv3s[1] = new SerializableVector3(10, 2, 3);
+        sV3List.sv3s[2] = new SerializableVector3(1, 20, 3);
+        sV3List.sv3s[3] = new SerializableVector3(1, 2, 30);
+
+        Debug.Log("XXXXXX: " + sV3List.sv3s.Length);
+        JObject jo = JObject.FromObject(sV3List);
+        Debug.Log("XXXXXX: " + jo);
+
+        return JObject.FromObject(sV3List);
+    }
+
+
+    private JObject Shapes2JSON()
+    {
+        ShapeList shapeList = new ShapeList();
+        shapeList.shapes = new ShapeInfo[shapeInfoList.Count];
+        for (int i = 0; i < shapeInfoList.Count; i++)
+        {
+            shapeList.shapes[i] = shapeInfoList[i];
+        }
+
+        return JObject.FromObject(shapeList);
+    }
+
+
+    private void LoadSv3ListJSON(JToken mapMetadata)
+    {
+        paintManager.currVertices.Clear();
+
+        if (mapMetadata is JObject && mapMetadata["sv3list"] is JObject)
+        {
+            SV3List sv3list = mapMetadata["sv3list"].ToObject<SV3List>();
+            if (sv3list.sv3s == null)
+            {
+                Debug.Log("no sv3s dropped");
+                return;
+            }
+
+            foreach (SerializableVector3 sv3 in sv3list.sv3s)
+            {
+                Vector3 vector = sv3;
+                Debug.Log("YYYYY " + sv3);
+                paintManager.currVertices.Add(vector);
+            }
+        }
+    }
+
+
+    private void LoadShapesJSON(JToken mapMetadata)
+    {
+        ClearShapes();
+
+        if (mapMetadata is JObject && mapMetadata["shapeList"] is JObject)
+        {
+            ShapeList shapeList = mapMetadata["shapeList"].ToObject<ShapeList>();
+            if (shapeList.shapes == null)
+            {
+                Debug.Log("no shapes dropped");
+                return;
+            }
+
+            foreach (var shapeInfo in shapeList.shapes)
+            {
+                shapeInfoList.Add(shapeInfo);
+                GameObject shape = ShapeFromInfo(shapeInfo);
+                shapeObjList.Add(shape);
+            }
+        }
+    }
+
+
+    public void OnPose(Matrix4x4 outputPose, Matrix4x4 arkitPose) { }
+
+
+    public void OnStatusChange(LibPlacenote.MappingStatus prevStatus,
+                                LibPlacenote.MappingStatus currStatus)
+    {
+        Debug.Log("prevStatus: " + prevStatus.ToString() +
+                   " currStatus: " + currStatus.ToString());
+        if (currStatus == LibPlacenote.MappingStatus.RUNNING &&
+            prevStatus == LibPlacenote.MappingStatus.LOST)
+        {
+            mLabelText.text = "Localized";
+            LoadShapesJSON(mSelectedMapInfo.userData);
+            LoadSv3ListJSON(mSelectedMapInfo.userData);
+        }
+        else if (currStatus == LibPlacenote.MappingStatus.RUNNING &&
+                 prevStatus == LibPlacenote.MappingStatus.WAITING)
+        {
+            mLabelText.text = "Mapping";
+        }
+        else if (currStatus == LibPlacenote.MappingStatus.LOST)
+        {
+            mLabelText.text = "Searching for position lock";
+        }
+        else if (currStatus == LibPlacenote.MappingStatus.WAITING)
+        {
+            if (shapeObjList.Count != 0)
+            {
+                ClearShapes();
+            }
+        }
+    }
+}
