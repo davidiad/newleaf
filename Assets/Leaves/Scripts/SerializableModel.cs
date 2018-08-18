973df2afe9fe66ec85c55fdaf71bd42e23e75aa0
David Fierstein <studio@davidiad.com>
2018-08-17 22:13:33 -0700

    Continue SerializableModel
    Change confusing terminology (e.g., naming an object that holds an array, a ModelInfoList). There are List versions of the same info, hence the confusion.

--- Assets/Leaves/Scripts/SerializableModel.cs
+++ Assets/Leaves/Scripts/SerializableModel.cs
@@ -17,30 +17,32 @@ public class ModelInfo
 }
 
 [Serializable]
-public class ModelList
+public class ModelInfoArray // change  name, because not a List
 {
-    public ModelInfo[] models;
+    public ModelInfo[] modelInfos;
 }
-
 
 public class SerializableModel : ScriptableObject {
 
-    public GameObject[] prefabs;
+    public GameObject[] prefabs; // models to choose from
     //TODO: load the prefabs
-    
-    private JObject toJSON(List<ModelInfo> modelInfoList)
+    private List<ModelInfo> modelInfoList = new List<ModelInfo>(); // need to pass in
+    private List<GameObject> modelObjList = new List<GameObject>();
+
+    // convert array of model info to json
+    private JObject ToJSON()
     {
-        ModelList modelList = new ModelList();
-        modelList.models = new ModelInfo[modelInfoList.Count];
+        ModelInfoArray modelInfoArray = new ModelInfoArray();
+        modelInfoArray.modelInfos = new ModelInfo[modelInfoList.Count];
         for (int i = 0; i < modelInfoList.Count; i++)
         {
-            modelList.models[i] = modelInfoList[i];
+            modelInfoArray.modelInfos[i] = modelInfoList[i];
         }
 
-        return JObject.FromObject(modelList);
+        return JObject.FromObject(modelInfoArray);
     }
 
-    // Add a custom 3D model
+    // get a custom 3D model
     private GameObject ModelFromInfo(ModelInfo info)
     {
         Vector3 pos = new Vector3(info.px, info.py, info.pz);
@@ -49,6 +51,32 @@ public class SerializableModel : ScriptableObject {
         GameObject model = Instantiate(prefabs[info.modelIndex], pos, rot);
 
         return model;
+    }
+
+    public void OnAddToScene(ModelInfo info)
+    {
+        // get the object transform info to use
+        Vector3 pos = Camera.main.transform.position + Camera.main.transform.forward * 1.3f;
+        Quaternion rot = Camera.main.transform.rotation;
+
+        // put the transform info into model info object
+        info.px = pos.x;
+        info.py = pos.y;
+        info.pz = pos.z;
+        info.qx = rot.x;
+        info.qy = rot.y;
+        info.qz = rot.z;
+        info.qw = rot.w;
+        info.modelIndex = 0; // modelIndex should already be in the info parameter
+
+        // add info to info list
+        modelInfoList.Add(info);
+
+        // Instantiate and add to scene
+        GameObject model = ModelFromInfo(info);
+
+        // add the game object to object list
+        modelObjList.Add(model);
     }
 
 }
