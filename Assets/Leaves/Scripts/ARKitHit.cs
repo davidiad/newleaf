42b6c651c59b3aab64abcfe1cd9c3a541dca1c5c
David Fierstein <studio@davidiad.com>
2018-09-05 23:15:49 -0700

    Extend depth range of plane painting
    Experiment with depth cues.

--- Assets/Leaves/Scripts/ARKitHit.cs
+++ Assets/Leaves/Scripts/ARKitHit.cs
@@ -90,119 +90,129 @@ namespace UnityEngine.XR.iOS
 
         void Update()
         {
-            // detect hit on plane in front of camera, or one of the other grid planes associated with a paintstroke
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            { // Block if over UI element
+              // detect hit on plane in front of camera, or one of the other grid planes associated with a paintstroke
 
-            // for Any state of a touch, get the transform of the hit
-            if (paintManager.paintOnTouch && Input.touchCount > 0 && !paintManager.ARPlanePainting)
-            {
-                if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) // Block if over UI element
-                { 
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    RaycastHit hit;
-                    // try to hit plane collider gameobjects attached to camera
-                    // effectively similar to calling HitTest with ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent
-
-                    //TODO: Set up 2 raycasts. First will raycast against a Grid layer, and find the first grid hit 
-                    // (not the camera grid, which will be on a different layer)
-                    // If there are no results from that first raycast, then raycast against the camera grid layer
-
-                    // first, check if there are any grids (not cam grid) being hit
-                    // If a grid is found, use that exisiting grid
-                    // TODO: Add a grid property to each paintstroke, and associate that property with that paintstroke
-                    // There could/will be mulitple paintstrokes for each grid
-
-                    // Raycast against Grid layer, and find the first grid hit
-                    if (Physics.Raycast(ray, out hit, maxRayDistance, gridLayer)) 
-                    {
-                        // Get the position from the contact point
-                        m_HitTransform.position = hit.point;
-
-                        // and the rotation from the transform of the plane collider
-                        m_HitTransform.rotation = hit.transform.rotation;
-
-                        if (!planePainting) { PaintPlaneOn(); }
-                    }
-
-                    // No other grids found, so cast against the grid attached to the camera
-                    // Assuming cam grid is found (usually it will be), a new grid will be created, and then detached from the camera
-                    else
-                        if (Physics.Raycast(ray, out hit, maxRayDistance, cameraGridLayer)) 
-                    {
-                        m_HitTransform.position = hit.point;
-                        m_HitTransform.rotation = hit.transform.rotation;
-                        if (!planePainting) { PaintPlaneOn(); }
-
-                        // Deparent the plane that's been hit, so it is stationary in world space
-                        camPaintingPlane.transform.SetParent(null);
-                        // Convert camPaintingPlane to GridPlane. There will be only one camPaintingPlane at a time, but could be many Grids
-                        // A new camPaintingPlane will be created when this paintstroke is ended
-                        camPaintingPlane.tag = "Grid";
-                        camPaintingPlane.layer = 10; // the int of the Grid layer
-                        camPaintingPlane.GetComponent<MeshRenderer>().enabled = true;
-                    }
-                }
-            }
-
-            if (Input.touchCount > 0 && m_HitTransform != null)
-            {
-                var touch = Input.GetTouch(0);
-                if (touch.phase == TouchPhase.Began) {
-					
-                    previousRadius = touch.radius;
-                }
-                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+                // for Any state of a touch, get the transform of the hit
+                if (paintManager.paintOnTouch && Input.touchCount > 0 && !paintManager.ARPlanePainting)
                 {
-                    // Control size of brush with touch
-                    // limit max allowed change from previous frame to keep size transitions smooth
-                    float allowedRadius = touch.radius;
-                    if ((touch.radius - previousRadius) > maxAllowedSizeChange) {
-                        allowedRadius = previousRadius + maxAllowedSizeChange;
-                    } else if ((touch.radius - previousRadius) < (-1f * maxAllowedSizeChange)) {
-                        allowedRadius = previousRadius - maxAllowedSizeChange;
-                    }
-                    previousRadius = allowedRadius; // reset in prep for next frame
-                    // radius usually in range of 20 to 40, as low as 10, as high as 200
-                    // attempt to bring brush size to range of 1/4 cm to 10 cm
-                    float adjustedRadius = allowedRadius * 0.002f;
-                    // if Apple pencil is being used, use the amount of pressure instead
-                    if (touch.type == TouchType.Stylus) { adjustedRadius = touch.pressure * 0.04f; }
-                    paintManager.brushSize = adjustedRadius * adjustedRadius;
-
-                    paintManager.AdjustBrushSize();
-
-                    // This section is needed for painting on AR planes, but interferes with painting on Camera plane (hence the if statement)
-                    if (paintManager.ARPlanePainting && touch.phase == TouchPhase.Moved && touch.phase != TouchPhase.Stationary) // try to avoid beginning touch which is off the plane
+                    if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) // Block if over UI element
                     {
-                        var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
-                        ARPoint point = new ARPoint
-                        {
-                            x = screenPosition.x,
-                            y = screenPosition.y
-                        };
+                        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+                        // try to hit plane collider gameobjects attached to camera
+                        // effectively similar to calling HitTest with ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent
 
-                        ARHitTestResultType[] resultTypes = { ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent };
-                        foreach (ARHitTestResultType resultType in resultTypes)
+                        //TODO: Set up 2 raycasts. First will raycast against a Grid layer, and find the first grid hit 
+                        // (not the camera grid, which will be on a different layer)
+                        // If there are no results from that first raycast, then raycast against the camera grid layer
+
+                        // first, check if there are any grids (not cam grid) being hit
+                        // If a grid is found, use that exisiting grid
+                        // TODO: Add a grid property to each paintstroke, and associate that property with that paintstroke
+                        // There could/will be mulitple paintstrokes for each grid
+
+                        // Raycast against Grid layer, and find the first grid hit
+                        if (Physics.Raycast(ray, out hit, maxRayDistance, gridLayer))
                         {
-                            // returns the 1st point hit casting from screenpoint to an arplane
-                            if (HitTestWithResultType(point, resultType)) { return; }
+                            // Get the position from the contact point
+                            m_HitTransform.position = hit.point;
+
+                            // and the rotation from the transform of the plane collider
+                            m_HitTransform.rotation = hit.transform.rotation;
+
+                            if (!planePainting) { PaintPlaneOn(); }
+                        }
+                        // No other grids found, so cast against the grid attached to the camera
+                        // Assuming cam grid is found (usually it will be), a new grid will be created, and then detached from the camera
+                        else if (Physics.Raycast(ray, out hit, maxRayDistance, cameraGridLayer))
+                        {
+                            m_HitTransform.position = hit.point;
+                            m_HitTransform.rotation = hit.transform.rotation;
+                            if (!planePainting) { PaintPlaneOn(); }
+
+                            // Deparent the plane that's been hit, so it is stationary in world space
+                            camPaintingPlane.transform.SetParent(null);
+                            // Convert camPaintingPlane to GridPlane. There will be only one camPaintingPlane at a time, but could be many Grids
+                            // A new camPaintingPlane will be created when this paintstroke is ended
+                            camPaintingPlane.tag = "Grid";
+                            camPaintingPlane.layer = 10; // the int of the Grid layer
+                            camPaintingPlane.GetComponent<MeshRenderer>().enabled = true;
                         }
                     }
-                } else if (touch.phase == TouchPhase.Ended) 
+                }
+
+                if (Input.touchCount > 0 && m_HitTransform != null)
                 {
-                    if (paintTarget.transform.parent.gameObject.CompareTag("PlanePainter"))
+                    var touch = Input.GetTouch(0);
+                    if (touch.phase == TouchPhase.Began)
                     {
-                        if (planePainting) { 
-                            PaintPlaneOff(); 
-                            paintManager.paintOnTouch = true; 
+
+                        previousRadius = touch.radius;
+                    }
+                    if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved)
+                    {
+                        // Control size of brush with touch
+                        // limit max allowed change from previous frame to keep size transitions smooth
+                        float allowedRadius = touch.radius;
+                        if ((touch.radius - previousRadius) > maxAllowedSizeChange)
+                        {
+                            allowedRadius = previousRadius + maxAllowedSizeChange;
                         }
-                        // Now create a new Camera painting grid
-                        paintManager.AddPaintingPlaneToCam();
-                        camPaintingPlane = GameObject.FindWithTag("CamPaintingPlane");
-                        /*// ensure that there is no brush attached to the PaintTarget
-                        foreach (Transform child in paintTarget.transform) {
-                            if (child.name.Contains("Triangle-for-painting")) {
+                        else if ((touch.radius - previousRadius) < (-1f * maxAllowedSizeChange))
+                        {
+                            allowedRadius = previousRadius - maxAllowedSizeChange;
+                        }
+                        previousRadius = allowedRadius; // reset in prep for next frame
+                                                        // radius usually in range of 20 to 40, as low as 10, as high as 200
+                                                        // attempt to bring brush size to range of 1/4 cm to 10 cm
+                        float adjustedRadius = allowedRadius * 0.002f;
+                        // if Apple pencil is being used, use the amount of pressure instead
+                        if (touch.type == TouchType.Stylus) { adjustedRadius = touch.pressure * 0.04f; }
+                        paintManager.brushSize = adjustedRadius * adjustedRadius;
+
+                        paintManager.AdjustBrushSize();
+
+                        // This section is needed for painting on AR planes, but interferes with painting on Camera plane (hence the if statement)
+                        if (paintManager.ARPlanePainting && touch.phase == TouchPhase.Moved && touch.phase != TouchPhase.Stationary) // try to avoid beginning touch which is off the plane
+                        {
+                            var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+                            ARPoint point = new ARPoint
+                            {
+                                x = screenPosition.x,
+                                y = screenPosition.y
+                            };
+
+                            ARHitTestResultType[] resultTypes = { ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent };
+                            foreach (ARHitTestResultType resultType in resultTypes)
+                            {
+                                // returns the 1st point hit casting from screenpoint to an arplane
+                                if (HitTestWithResultType(point, resultType)) { return; }
+                            }
+                        }
+                    }
+                    else if (touch.phase == TouchPhase.Ended)
+                    {
+                        if (paintTarget.transform.parent.gameObject.CompareTag("PlanePainter") && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                        {
+                            if (planePainting)
+                            {
+                                PaintPlaneOff();
+                                paintManager.paintOnTouch = true;
+                            }
+
+
+                            // now create a new Camera painting grid
+                            paintManager.AddPaintingPlaneToCam();
+                            camPaintingPlane = GameObject.FindWithTag("CamPaintingPlane");
+                            /*// ensure that there is no brush attached to the PaintTarget
+                                foreach (Transform child in paintTarget.transform) {
+                                if (child.name.Contains("Triangle-for-painting")) {
                                 Destroy(child);
                             } */
+
+                        }
                     }
                 }
             }
