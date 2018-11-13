c507aa6bca821ba407f5e1b1958d8dbb5435d333
David Fierstein <studio@davidiad.com>
2018-11-12 23:23:25 -0800

    Create DownloadManager and DownloadBar

---
+++ Assets/Leaves/Scripts/DownloadManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//TODO: should be a UI element
+public class DownloadBar : MonoBehaviour
+{
+    public GameObject prefab;
+    private GameObject bar;
+    public float progress { get; set; }
+    public bool finished { get; set;  }
+    public void UpdateDisplay () 
+    {
+        
+    }
+}
+
+public class DownloadManager : MonoBehaviour {
+
+
+}
