f4240364425a943b6532858c94f0489b46924464
David Fierstein <studio@davidiad.com>
2018-12-20 12:54:27 -0800

    Color wheel - implement dragging

---
+++ Assets/Leaves/Scripts/ColorWheel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorWheel : MonoBehaviour {
+
+    private float rotZ;
+    private RectTransform rectTransform;
+	void Start () 
+    {
+        rectTransform = GetComponent<RectTransform>();
+	}
+	
+	public void ChangeColor () 
+    {
+        rectTransform.Rotate(0f, 0f, 1f);
+        rotZ = rectTransform.rotation.eulerAngles.z;
+        Debug.Log("Color Wheel Rotation: " + rotZ);
+
+	}
+}
---
+++ Assets/Leaves/Scripts/TransparentButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TransparentButton : MonoBehaviour {
+
+	void Start () {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+	}
+}
