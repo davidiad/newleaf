7ce2a0eb12cb4b4ac039c8f042ad298e5baac450
David Fierstein <studio@davidiad.com>
2018-12-24 22:23:03 -0800

    Add EventSystem interfaces to ColorWheel
    Begin implementation of drag handlers to translate drag into circular motion and changing current color.

--- Assets/Leaves/Scripts/ColorWheel.cs
+++ Assets/Leaves/Scripts/ColorWheel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ColorWheel : MonoBehaviour {
 
@@ -15,7 +13,38 @@ public class ColorWheel : MonoBehaviour {
     {
         rectTransform.Rotate(0f, 0f, 1f);
         rotZ = rectTransform.rotation.eulerAngles.z;
-        Debug.Log("Color Wheel Rotation: " + rotZ);
-
+        //Debug.Log("Color Wheel Rotation: " + rotZ);
 	}
+
+    private float CalculateRotationFromDrag()
+    {
+        Event e = Event.current;
+        print("MOUSE POS START");
+        Debug.Log(e.mousePosition);
+        print("__________END");
+
+        // Get the current center of the colorwheel.
+        //float c = rectTransform.
+        // Vector2 startVector = Get the screen position of the center. Get the screen pos of start of drag
+
+        return 0f; 
+
+        // 1. Get the vector from local color wheel center to drag start location
+
+        // 2. Each frame while dragging: get the vector from center to drag position
+
+        // 3. Find the difference in angle of these 2 vectors.
+
+        // 4. Rotate that amount
+
+        // 5. Change the color to the corresponding hue
+
+        // 6. ? Need to reset/check for alignment to correct colors periodically?
+
+        //*********Reference************
+        // public static float SignedAngle(Vector2 from, Vector2 to);
+        // The result is never greater than 180 degrees or smaller than -180 degrees.
+
+
+    }
 }
---
+++ Assets/Leaves/Scripts/ColorWheelEventHandlers.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColorWheelEventHandlers : MonoBehaviour, IPointerEnterHandler, IDragHandler, IBeginDragHandler, IPointerExitHandler, IDropHandler
+{
+    Touch touch;
+
+    void Start()
+    {
+        //touch = new Touch();
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("e.pos: " + eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //if (Input.touchCount > 0)
+        //{
+            //touch = Input.GetTouch(0);
+            this.gameObject.transform.position = eventData.position;  //touch.position;
+        //}
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+}
