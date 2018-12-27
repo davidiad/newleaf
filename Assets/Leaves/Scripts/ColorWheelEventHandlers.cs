c6f0e07642e24b78d8237cb661ef4f78e08dc9a0
David Fierstein <studio@davidiad.com>
2018-12-27 00:05:09 -0800

    Fix ColorWheel drag handler vector

--- Assets/Leaves/Scripts/ColorWheelEventHandlers.cs
+++ Assets/Leaves/Scripts/ColorWheelEventHandlers.cs
@@ -7,12 +7,14 @@ public class ColorWheelEventHandlers : MonoBehaviour, IPointerEnterHandler, IDra
 {
     //Touch touch;
 
+    private Camera cam;
     private Vector3 wheelPos;
     private float rotZ;
     private RectTransform rectTransform;
 
     void Start()
     {
+        cam = Camera.main;
         rectTransform = GetComponent<RectTransform>();
 
         //touch = new Touch();
@@ -32,9 +34,12 @@ public class ColorWheelEventHandlers : MonoBehaviour, IPointerEnterHandler, IDra
             //touch = Input.GetTouch(0);
             //this.gameObject.transform.position = eventData.position;  //touch.position;
         //}
-        Vector3 pos = new Vector3(eventData.position.x, eventData.position.y, wheelPos.z);
-        Debug.DrawRay(wheelPos, pos.normalized * 200f, Color.red, 0.2f);
-        Debug.Log("e.pos: " + eventData.position);
+        Vector3 pos = new Vector3(eventData.position.x, eventData.position.y, cam.nearClipPlane);
+        //Vector3 p = cam.ScreenToWorldPoint(pos);
+        Vector3 dir = pos - wheelPos;
+        //Debug.DrawRay(wheelPos, pos.normalized * 200f, Color.red, 0.2f);
+        Debug.DrawRay(wheelPos, dir, Color.red, 0.2f);
+
     }
 
     public void OnDrop(PointerEventData eventData)
